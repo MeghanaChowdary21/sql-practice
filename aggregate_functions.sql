ab55bb26b71b7ff3595d5bd98251db21b58b98d6
MeghanaChowdary21 <saimeghanachowdary21@gmail.com>
2026-08-02 13:30:42 +0530

    Add aggregate function examples

---
+++ aggregate_functions.sql
@@ -0,0 +1,11 @@
+SELECT COUNT(*) AS TotalEquipment
+FROM Equipment;
+
+SELECT COUNT(*) AS TotalDepartments
+FROM Departments;
+
+SELECT COUNT(*) AS TotalSuppliers
+FROM Suppliers;
+
+SELECT COUNT(*) AS TotalMaintenanceRecords
+FROM Maintenance;
